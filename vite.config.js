97fc88732c6472ced7b8c15837cdd2aa8d44f6ad
Nirmala Jadhav <96299458+Nirmala-25@users.noreply.github.com>
2024-06-26 16:12:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-dotenv.config();
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000,
-    strictPort: true
-  },
-  preview: {
-    port: process.env.PORT || 4173,
-    strictPort: true
-  }
-});
+})
